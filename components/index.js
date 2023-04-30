da74026b2aa4dc14393c3b4afdccbb5e99b1d597
walidmeguenni <walidmg125@gmail.com>
2023-04-30 17:51:25 +0100

    set up dashboard

---
+++ components/index.js
@@ -0,0 +1,4 @@
+import Navbar from "./navbar";
+import Layout from "./layout";
+
+export { Navbar, Layout };
---
+++ pages/mininig.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Mininig = () => {
+  return (
+    <div>Mininig</div>
+  )
+}
+
+export default Mininig
